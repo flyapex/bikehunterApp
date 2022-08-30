2ee3e10e383e682969cc1d29ab2ef07917bff430
Ahad <86792385+Ahad-Haque@users.noreply.github.com>
2022-08-30 18:01:14 +0600

    added geo locator and google maps lib

--- lib/screen/home/widget/location_sheet.dart
+++ lib/screen/home/widget/location_sheet.dart
@@ -7,6 +7,7 @@ import 'package:bikehunter/model/location_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationAppbarBottomSheet extends StatelessWidget {
   const LocationAppbarBottomSheet({Key? key}) : super(key: key);
